4666f235407b566258f2a445da15a11c898e7174
yapabandara_7 <dhanuyapaugc@gmail.com>
2024-07-19 08:55:16 +0530

    created calculator

--- calculator/apps/calculator/src/app/app.controller.ts
+++ calculator/apps/calculator/src/app/app.controller.ts
@@ -1,13 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Post('/hello')
+  postHelloMessage() {
+    // Static message return
+    return { message: "Hello API" };
   }
+
+  @Post('/addition')
+  postData(@Body() body: { number1: number, number2: number }) {
+    // Dynamically calculate sum based on input
+    return this.appService.addition(body.number1, body.number2);
+  }
+
+  @Post('/subtraction')
+  postData2(@Body() body: { number1: number, number2: number }) {
+    // Dynamically calculate sum based on input
+    return this.appService.subtraction(body.number1, body.number2);
+  }
+
+
+  @Post('/multiplication')
+  postData3(@Body() body: { number1: number, number2: number }) {
+    // Dynamically calculate sum based on input
+    return this.appService.multiplication(body.number1, body.number2);
+  }
+
+  @Post('/division')
+  postData4(@Body() body: { number1: number, number2: number }) {
+    // Dynamically calculate sum based on input
+    return this.appService.division(body.number1, body.number2);
+  }
+  @Post('/division')
+  postData5(@Body() body: { number1: number, number2: number }) {
+    // Dynamically calculate sum based on input
+    return this.appService.division(body.number1, body.number2);
+  }
+
 }
--- calculator/apps/calculator/src/app/app.service.ts
+++ calculator/apps/calculator/src/app/app.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return ({ message: 'Hello API' });
-  }
+
+
+  addition(number1: number, number2: number) {
+    return { result: number1 + number2 };
+}
+
+subtraction(number1: number, number2: number) {
+  return { result: number1 - number2 };
+}
+multiplication(number1: number, number2: number) {
+  return { result: number1 * number2 };
+}
+
+division(number1: number, number2: number) {
+  return { result: number1 / number2 };
+}
+
 }
